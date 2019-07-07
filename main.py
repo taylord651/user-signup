5685885b70173835ae7220125a103e944451efc1
Domonique Taylor <taylord651@gmail.com>
2019-07-07 16:37:37 -0500

    Final code with full functionality (change to redirect command to show /welcome in web address

--- main.py
+++ main.py
@@ -6,13 +6,71 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
-@app.route("/signup", methods=['POST'])
-def signup():
+@app.route("/signup")
+def display_form():
     return render_template("signup.html")
 
-@app.route("/welcome", methods=["POST"])
-def welcome():
+@app.route("/", methods=['POST'])
+@app.route("/signup", methods=['POST'])
+def validate_signup():
+
     username = request.form['username']    
-    return render_template("welcome.html", username=username)
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+
+    username_error = ""
+    password_error = ""
+    verify_password_error = ""
+    email_error = ""
+    
+    if username == "":
+        username_error = "Error: username required"
+    else:
+        if len(username) < 3 or len(username) > 20:
+            username_error = "Error: please type a username that is 3-20 characters long"
+            username = ""
+
+    if password == "":
+        password_error = "Error: password required"
+        password = ""
+    else:
+        if len(password) < 3 or len(password) > 20:
+            password_error = "Error: please type a password that is 3-20 characters long"
+            password = ""
+
+    if verify_password == "":
+        verify_password_error = "Error: please verify your password"
+        verify_password = ""
+    else:
+        if password != verify_password:
+            verify_password_error = "Error: Password and Verify Password do not match"
+            verify_password = ""
+
+    if email != "":
+        if len(email) < 3 or len(email) > 20:
+            email_error = 'Error: Email is not valid. It must be 3-20 characters long.'
+        elif " " in email:
+            email_error = 'Error: Email is not valid. It must have no spaces.'
+        else:
+            if email != "" and "@" not in email and "." not in email:
+                email_error = 'Error: Email is not valid. It must contain an "@" symbol, a "."'
+
+    if not username_error and not password_error and not verify_password_error and not email_error:
+        return render_template("welcome.html", username=username)
+    else:
+        return render_template("signup.html",
+            username = username,
+            email = email,
+            username_error = username_error, 
+            password_error = password_error, 
+            verify_password_error = verify_password_error, 
+            email_error = email_error)
+
+
+@app.route("/welcome", methods=["POST", "GET"])
+def welcome():
+    username = request.form['username']   
+    return render_template("/welcome", username=username)
 
 app.run()
